8b86754fcc3f60b65fac4e30008fb9947d64cf0a
Abdulmalik Aremu <aremzy2018@gmail.com>
2025-01-31 15:11:58 +0100

    task 4,5 and 6 completed

--- task_4.sql
+++ task_4.sql
@@ -1,6 +1,4 @@
 USE alx_book_store;
-SHOW COLUMNS FROM `Books`;
-SHOW COLUMNS FROM `Authors`;
-SHOW COLUMNS FROM `Customers`;
-SHOW COLUMNS FROM `Orders`;
-SHOW COLUMNS FROM `Order_Details`;
+SELECT COLUMN_NAME, COLUMN_TYPE FROM information_schema.columns 
+WHERE TABLE_SCHEMA = 'alx_book_store'
+AND TABLE_NAME = 'Books';
