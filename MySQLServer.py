03bd4a13b92ac16acc120b4412232763fb43bc00
Abdulmalik Aremu <aremzy2018@gmail.com>
2025-01-31 13:38:48 +0100

    server created successfully

---
+++ MySQLServer.py
@@ -0,0 +1,19 @@
+import mysql.connector
+
+
+try:
+    mydb = mysql.connector.connect(
+    host = "localhost",
+    username = "root",
+    password = "incel123"
+    )
+except Exception:
+    print("Error connecting to database")
+
+mycursor = mydb.cursor()
+
+sql = "CREATE DATABASE IF NOT EXISTS alx_book_store"
+mycursor.execute(sql)
+print("Database 'alx_book_store' created successfully!")
+mycursor.close()
+mydb.close()
